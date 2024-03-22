19946ea7fd886915a8e3b99f94c59da029228d73
prashikubale <156569419+prashikubale@users.noreply.github.com>
2024-03-22 14:52:05 +0530

    Add files via upload

---
+++ mapscript.js
@@ -0,0 +1,9 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoicGVyc3VpcyIsImEiOiJjbHUyYndwdGswcTZyMnZxc3l3M2F1b3prIn0._d5y2VHnvvdhRB0uesZKaw';
+
+
+
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+});
